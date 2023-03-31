a99e8e2b23567b6eda937a275cc8b3057af679b3
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-03-30 20:17:37 -0600

    ajkf

--- src/Components/utils/axios.js
+++ src/Components/utils/axios.js
@@ -13,7 +13,7 @@ export let reqInstance = axios.create({
 	headers: {
 		"X-CSRFToken": token,
 	},
-	baseURL: `http://127.0.0.1:8000/`,
+	baseURL: `https://sociallydistributed.herokuapp.com/`,
 	auth: {
 		username: username,
 		password: password,
