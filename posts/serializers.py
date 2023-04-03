3cfd437e1c14fd94612b5595bd3e7b909f7538e1
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-04-03 13:14:27 -0600

    fafaf

--- posts/serializers.py
+++ posts/serializers.py
@@ -20,22 +20,22 @@ class PostSerializer(WritableNestedModelSerializer):
     def create(self, validated_data):
         print("validated post data",validated_data)
         try:
-            author = AuthorSerializer.extract_and_upcreate_author(validated_data['author'], None)
             post = Post.objects.create(**validated_data)
-            print("Create Post/ extract and upcreate")
         except:
             author = AuthorSerializer.extract_and_upcreate_author(None, author_id=self.context["author_id"])
             id = validated_data.pop('id') if validated_data.get('id') else None
             if not id:
                 id = self.context["id"]
             post = Post.objects.create(**validated_data, author = author, id = id)
-            print("except create post object")
-        print(post)
+
         return post
 
     def to_internal_value(self, data):
-        print("internal value")
-        data["categories"] = ','.join(data["categories"])
+        try: 
+            data["categories"] = ','.join(data["categories"])
+            data["author"] = AuthorSerializer.extract_and_upcreate_author(data['author'], None)
+        except: 
+            pass
         return super().to_internal_value(data)
         
 
