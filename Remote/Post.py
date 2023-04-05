0944bdb0c4f2856076b73aef491992e39292bd8e
Leen-Alzebdeh <alzebdeh@ualberta.ca>
2023-04-04 23:44:49 -0600

    fix posts to yoshi-connect

--- Remote/Post.py
+++ Remote/Post.py
@@ -164,7 +164,7 @@ def sendPostYoshi(data, auth_id):
     if data["unlisted"]:
         data["unlisted"] = "true"
     #update the data to be sent in proper format maybe
-    response = requests.post(url=url, headers=yoshi_headers(), data=data)
+    response = requests.post(url=url, headers=yoshi_headers(), data=json.dumps(data))
     status_code = response.status_code
     json_response = response.json()
     print("YOSHI content", json_response)
