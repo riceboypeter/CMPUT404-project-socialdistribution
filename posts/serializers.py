3afe2917d10d907e26c426cd82ceb69ef5be4d14
Peter <sqiu@ualberta.ca>
2023-04-06 18:44:48 -0600

    f

--- posts/serializers.py
+++ posts/serializers.py
@@ -231,11 +231,12 @@ class ImageSerializer(WritableNestedModelSerializer):
             post = Post(**validated_data)
         except Exception as e:
             print(e)
-            print("image post serializer else")
+            print("in the except block")
             author = AuthorSerializer.extract_and_upcreate_author(None, author_id=self.context["author_id"])
+            print(author)
             validated_data.pop('authors')
             print("validated data ",validated_data)
-            post = Post.objects.create(**validated_data, author = author, id = id)
+            post = Post.objects.create(**validated_data, author = author)
         return post
     
     def to_internal_value(self, data):
