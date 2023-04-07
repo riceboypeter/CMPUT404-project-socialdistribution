0aedbede8d192fa1199d5fcdc3c15cddc676ca50
hbheesetti <77315050+hbheesetti@users.noreply.github.com>
2023-04-06 23:32:49 -0600

    fafaf

--- Remote/auth.py
+++ Remote/auth.py
@@ -24,7 +24,7 @@ def yoshi_headers():
     credentials = f'{username}:{password}'
     encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
     authorization_header = f'Basic {encoded_credentials}'
-    headers = {'Authorization': authorization_header}
+    headers = {'Authorization': authorization_header, 'Content-Type': 'application/json'}
     return headers
 
 def p2_headers():
